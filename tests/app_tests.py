2abf6cf7a3501d79fbfecb8089a27b0b9429bda4
RyanBeatty <rvbeatty@email.wm.edu>
2015-05-03 23:41:39 -0400

    created basic first test

---
+++ tests/app_tests.py
@@ -0,0 +1,11 @@
+import app
+import unittest
+
+class SteerClearTestCase(unittest.TestCase):
+	def setUp(self):
+		app.app.config['TESTING'] = True
+		self.app = app.app.test_client()
+
+	def test_heartbeat(self):
+		response = self.app.get('/')
+		self.assertEquals(response.status, "200 OK")
