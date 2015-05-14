1a18cb99ddc2e40251b3d881b59655a76ce4dee9
RyanBeatty <rvbeatty@email.wm.edu>
2015-05-14 02:14:02 -0400

    added sqlalchemy initial integration. Created Ride model in models.py. Can list the ride queue. needs ALOT of refactoring

--- steerclear/__init__.py
+++ steerclear/__init__.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask.ext.sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
 from steerclear import views
--- steerclear/views.py
+++ steerclear/views.py
@@ -1,5 +1,22 @@
 from steerclear import app
+from flask import request, json
+from models import *
+
+def list_rides():
+	result = Ride.query.all()
+	return map(Ride.as_dict, result)
+
+
+
 
 @app.route('/')
 def heartbeat():
         return "pulse"
+
+@app.route('/rides', methods=['GET', 'POST'])
+def rides():
+	if request.method == 'GET':
+		return json.jsonify({'rides': list_rides()})
+	else:
+		return "new ride"
+
