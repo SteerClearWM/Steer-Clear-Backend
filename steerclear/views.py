ccb3bc119e75cbeb30fa3eefdd1f66fc1435e0f6
RyanBeatty <rvbeatty@email.wm.edu>
2015-05-14 02:38:34 -0400

    initial code for adding rides done. needs refactoring

--- steerclear/views.py
+++ steerclear/views.py
@@ -1,12 +1,16 @@
 from steerclear import app
-from flask import request, json
+from flask import request, json, abort
 from models import *
 
 def list_rides():
 	result = Ride.query.all()
 	return map(Ride.as_dict, result)
 
-
+def add_ride(name, phone_number):
+	new_ride = Ride(name, phone_number)
+	db.session.add(new_ride)
+	db.session.commit()
+	return new_ride.as_dict()
 
 
 @app.route('/')
@@ -17,6 +21,14 @@ def heartbeat():
 def rides():
 	if request.method == 'GET':
 		return json.jsonify({'rides': list_rides()})
-	else:
-		return "new ride"
+	
+	print request.form
+	name = request.form.get('name', '')
+	print "name: " + name
+	phone_number = request.form.get('phone_number', '')
+	if not name or not phone_number:
+		abort(404)
+
+	new_ride = add_ride(name, phone_number)
+	return json.jsonify({'ride': new_ride})
 
