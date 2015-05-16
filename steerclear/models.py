2e990e114cd5b5f4f27061f046f4d6a55144ffa3
RyanBeatty <rvbeatty@email.wm.edu>
2015-05-16 00:24:25 -0400

    added test to check if add_ride works. changed Ride model field naming

--- steerclear/models.py
+++ steerclear/models.py
@@ -9,21 +9,21 @@ class Ride(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     phone_number = db.Column(db.String(20))
     num_passengers = db.Column(db.Integer)
-    start_lat = db.Column(db.Float)
-    start_long = db.Column(db.Float)
-    end_lat = db.Column(db.Float)
-    end_long = db.Column(db.Float)
+    start_latitude = db.Column(db.Float)
+    start_longitude = db.Column(db.Float)
+    end_latitude = db.Column(db.Float)
+    end_longitude = db.Column(db.Float)
 
     def __init__(self, phone_number, num_passengers, pickup, dropoff):
         self.phone_number = phone_number
         self.num_passengers = num_passengers
-        self.start_lat, self.start_long = pickup
-        self.end_lat, self.end_long = dropoff
+        self.start_latitude, self.start_longitude = pickup
+        self.end_latitude, self.end_longitude = dropoff
 
     def __repr__(self):
         return "<Ride(Phone %r, Passengers %r, Pickup <%r, %r>, Dropoff <%r, %r>)>" % \
-                (self.phone_number, self.num_passengers, self.start_lat, self.start_long,
-                 self.end_lat, self.end_long)
+                (self.phone_number, self.num_passengers, self.start_latitude, self.start_longitude,
+                 self.end_latitude, self.end_longitude)
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
--- tests/app_tests.py
+++ tests/app_tests.py
@@ -2,24 +2,73 @@ from steerclear import app, db
 from steerclear.models import *
 import unittest, tempfile, json
 
+"""
+SteerClearTestCase
+------------------
+TestCase for testing all api routes
+"""
 class SteerClearTestCase(unittest.TestCase):
-	def setUp(self):
-		_, db_abs_path = tempfile.mkstemp()
-		app.config['TESTING'] = True
-		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
-		self.client = app.test_client()
-		db.create_all()
 
-	def tearDown(self):
-		db.session.remove()
-		db.drop_all()
+    """
+    setUp
+    -----
+    called before each test function. Sets up flask app test
+    instance and creates new test database
+    """
+    def setUp(self):
+        _, db_abs_path = tempfile.mkstemp()
+        app.config['TESTING'] = True
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+        self.client = app.test_client()
+        db.create_all()
 
-	def test_heartbeat(self):
-		response = self.client.get('/')
-		self.assertEquals(response.status_code, 200)
-		self.assertEquals(response.get_data(), "pulse")
+    """
+    tearDown
+    --------
+    called after each test function. breaks down test fixtures
+    """
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
 
-	def test_list_rides(self):
-		response = self.client.get('/rides')
-		self.assertEquals(response.status_code, 200)
-		self.assertEquals(json.loads(response.get_data()), {"rides": []})
+    """
+    test_heartbeat
+    --------------
+    tests that server is up and running and that the
+    '/' route returns a 200 status code and "pulse"
+    """
+    def test_heartbeat(self):
+        response = self.client.get('/')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.get_data(), "pulse")
+
+    """
+    test_list_rides
+    ---------------
+    Tests that listing all of the rides in the queue is correct.
+    """
+    def test_list_rides(self):
+        response = self.client.get('/rides')
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(json.loads(response.get_data()), {"rides": []})
+
+    """
+    test_add_ride
+    -------------
+    Tests that adding a new ride request works. Sends POST ride
+    request data to '/rides/' and checks if the response json object
+    is a valid ride request
+    """
+    def test_add_ride(self):
+        payload = {
+                    u"phone_number": u"123-456-7890",
+                    u"num_passengers": 3,
+                    u"start_latitude": 1.1,
+                    u"start_longitude": 2.2,
+                    u"end_latitude": 3.3,
+                    u"end_longitude": 4.4,
+                  }
+        response = self.client.post('/rides', data=payload)
+        payload[u'id'] = 1
+        self.assertEquals(response.status_code, 200)
+        self.assertEquals(json.loads(response.get_data()), {u"ride": payload})
