7cdb13207f61696252d531481a911970e92af874
RyanBeatty <rvbeatty@email.wm.edu>
2015-05-15 22:16:41 -0400

    setup test database and wrote test for listing rides

--- tests/app_tests.py
+++ tests/app_tests.py
@@ -1,12 +1,26 @@
-import steerclear
-import unittest
+from steerclear import app, db
+from steerclear.models import *
+import unittest, tempfile, json
 
 class SteerClearTestCase(unittest.TestCase):
 	def setUp(self):
-		steerclear.app.config['TESTING'] = True
-		self.app = steerclear.app.test_client()
+		_, db_abs_path = tempfile.mkstemp()
+		app.config['TESTING'] = True
+		app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+		self.client = app.test_client()
+		db.create_all()
+
+	def tearDown(self):
+		db.session.remove()
+		db.drop_all()
 
 	def test_heartbeat(self):
-		response = self.app.get('/')
-		self.assertEquals(response.status, "200 OK")
+		response = self.client.get('/')
+		self.assertEquals(response.status_code, 200)
 		self.assertEquals(response.data, "pulse")
+
+	def test_list_rides(self):
+		response = self.client.get('/rides')
+		self.assertEquals(response.status_code, 200)
+		self.assertEquals(json.loads(response.data), {"rides": []})
+
