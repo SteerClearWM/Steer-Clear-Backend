b2462a9f191379c1692bc9c677f24477bd74720a
RyanBeatty <rvbeatty@email.wm.edu>
2015-05-20 15:11:41 -0400

    created settins folder and moved config.py over

--- steerclear/__init__.py
+++ steerclear/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('config')
+app.config.from_object('steerclear.settings.config')
 db = SQLAlchemy(app)
 
 from steerclear import views
