08ab6072f460ed935e65c57d8e4ef5dc446ba9c7
RyanBeatty <rvbeatty@email.wm.edu>
2015-05-20 15:12:38 -0400

    renamed config.py to default_settings.py

--- steerclear/__init__.py
+++ steerclear/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config.from_object('steerclear.settings.config')
+app.config.from_object('steerclear.settings.default_settings')
 db = SQLAlchemy(app)
 
 from steerclear import views
