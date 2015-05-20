378a168352ebf4b0663ece7b66a4271bfae83564
RyanBeatty <rvbeatty@email.wm.edu>
2015-05-20 15:19:43 -0400

    configuration from environment works now

--- steerclear/__init__.py
+++ steerclear/__init__.py
@@ -3,6 +3,7 @@ from flask.ext.sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('steerclear.settings.default_settings')
+app.config.from_envvar('STEERCLEAR_SETTINGS')
 db = SQLAlchemy(app)
 
 from steerclear import views
---
+++ steerclear/settings/unix_settings.py
@@ -0,0 +1,2 @@
+SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/app.db'
+TEST_SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
