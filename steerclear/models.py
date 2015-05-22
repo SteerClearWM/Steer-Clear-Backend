d7f880c4db442ef10571e29e6fdc5ba692baa706
RyanBeatty <rvbeatty@email.wm.edu>
2015-05-21 22:51:47 -0400

    added pickup_time field to Ride objects

--- steerclear/models.py
+++ steerclear/models.py
@@ -1,6 +1,7 @@
 # from flask.ext.sqlalchemy import SQLAlchemy
 from steerclear import db
 import sqlalchemy.types as types
+from datetime import datetime
 
 """
 Model class for the Ride object
@@ -12,20 +13,23 @@ class Ride(db.Model):
     start_longitude = db.Column(db.Float)
     end_latitude = db.Column(db.Float)
     end_longitude = db.Column(db.Float)
+    pickup_time = db.Column(types.DateTime)
 
     def __init__(self, num_passengers, pickup, dropoff):
         self.num_passengers = num_passengers
         self.start_latitude, self.start_longitude = pickup
         self.end_latitude, self.end_longitude = dropoff
+        self.pickup_time = datetime.utcnow()
 
     def __repr__(self):
-        return "<Ride(Passengers %r, Pickup <%r, %r>, Dropoff <%r, %r>)>" % \
+        return "<Ride(Passengers %r, Pickup <%r, %r>, Dropoff <%r, %r>, ETA %r)>" % \
                 (
                     self.num_passengers, 
                     self.start_latitude, 
                     self.start_longitude,
                     self.end_latitude, 
-                    self.end_longitude
+                    self.end_longitude,
+                    self.pickup_time
                 )
 
     def as_dict(self):
