04597f2723c87ae2ce6ff447e79c0624374491bc
RyanBeatty <rvbeatty@email.wm.edu>
2015-05-22 22:59:27 -0400

    removed pickup_time field from Ride objects. prepare for better development of feature

--- steerclear/models.py
+++ steerclear/models.py
@@ -13,13 +13,11 @@ class Ride(db.Model):
     start_longitude = db.Column(db.Float)
     end_latitude = db.Column(db.Float)
     end_longitude = db.Column(db.Float)
-    pickup_time = db.Column(types.DateTime)
 
     def __init__(self, num_passengers, pickup, dropoff):
         self.num_passengers = num_passengers
         self.start_latitude, self.start_longitude = pickup
         self.end_latitude, self.end_longitude = dropoff
-        self.pickup_time = datetime.utcnow()
 
     def __repr__(self):
         return "<Ride(Passengers %r, Pickup <%r, %r>, Dropoff <%r, %r>, ETA %r)>" % \
@@ -29,7 +27,6 @@ class Ride(db.Model):
                     self.start_longitude,
                     self.end_latitude, 
                     self.end_longitude,
-                    self.pickup_time
                 )
 
     def as_dict(self):
