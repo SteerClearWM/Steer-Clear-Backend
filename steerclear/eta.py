b20985e7d4d64a52e2e8a92aafa819fba6ea5987
RyanBeatty <rvbeatty@email.wm.edu>
2015-05-31 18:58:24 -0500

    added tests for all api eta json parsing

--- steerclear/eta.py
+++ steerclear/eta.py
@@ -18,22 +18,81 @@ def build_distancematrix_url(origins, destinations):
     query = build_query(origins, destinations)
     return build_url(query)
 
-def calculate_eta(start, end):
-    url = build_distancematrix_url(start, end)
-    response = requests.get(url)
+def distancematrix_api(origins, destinations):
+    url = build_distancematrix_url(origins, destinations)
+    return requests.get(url)
+
+def get_element_eta(element):
+    if element is None:
+        return None
+    if element.get(u'status', u'') != u'OK':
+        return None
+    duration = element.get(u'duration', None)
+    if duration is None:
+        return None
+    return duration.get(u'value', None)
+
+def get_elements_eta(elements):
+    if elements is None:
+       return None
+    elements_eta_list = map(get_element_eta, elements)
+    if elements_eta_list == [] or None in elements_eta_list:
+        return None
+    return elements_eta_list
+
+def get_row_eta(row):
+    if row is None:
+        return None
+    return get_elements_eta(row.get(u'elements', None))
+
+def get_rows_eta(rows, num_queries):
+    if rows is None:
+        return None
+    if len(rows) != num_queries:
+        return None
+    eta_list = map(get_row_eta, rows)
+    if eta_list == [] or None in eta_list:
+        return None
+    return eta_list
+
+# def calculate_eta(origins, destinations):
+#     url = build_distancematrix_url(origins, destinations)
+#     response = requests.get(url)
+#     if response.status_code != requests.codes.ok:
+#         return None
+#     data = response.json()
+#     rows = data.get(u'rows', None)
+#     if rows is None:
+#         return None
+#     elements = rows[0].get(u'elements', None)
+#     if elements is None:
+#         return None
+#     ride = elements[0]
+#     if ride.get(u'status', u'') != u'OK':
+#         return None
+#     duration = ride.get(u'duration', None)
+#     if duration is None:
+#         return None
+#     return duration.get(u'value', None)
+
+def time_between_rides(cur_ride, next_ride):
+    origins = [
+        (cur_ride.end_latitude, cur_ride.end_longitude), 
+        (next_ride.start_latitude, next_ride.start_longitude)
+    ]
+    destinations = [
+        (next_ride.start_latitude, next_ride.start_longitude),
+        (next_ride.end_latitude, next_ride.end_longitude)
+    ]
+    response = distancematrix_api(origins, destinations)
     if response.status_code != requests.codes.ok:
         return None
     data = response.json()
     rows = data.get(u'rows', None)
-    if rows is None:
+    eta_list = get_rows_eta(rows, len(origins))
+    if eta_list is None:
         return None
-    elements = rows[0].get(u'elements', None)
-    if elements is None:
-        return None
-    ride = elements[0]
-    if ride.get(u'status', u'') != u'OK':
-        return None
-    duration = ride.get(u'duration', None)
-    if duration is None:
-        return None
-    return duration.get(u'value', None)
+    return {
+        'pickup_time_sec': eta_list[0][0],
+        'travel_time_sec': eta_list[1][1]
+    }
--- tests/eta_tests.py
+++ tests/eta_tests.py
@@ -12,6 +12,20 @@ using google's distancematrix api
 """
 class ETATestCase(unittest.TestCase):
     
+    def setUp(self):
+        self.element1 = {u'status': u'OK', u'duration': {u'value': 0}}
+        self.element2 = {u'status': u'OK', u'duration': {u'value': 100}}
+        self.elements1 = [self.element1]
+        self.elements2 = [self.element2]
+        self.elements = [self.element1, self.element2]
+        self.row1 = {u'elements': self.elements1}
+        self.row2 = {u'elements': self.elements2}
+        self.row = {u'elements': self.elements}
+        self.rows1 = [self.row1]
+        self.rows2 = [self.row2]
+        self.rows3 = [self.row1, self.row2]
+        self.rows = [self.row1, self.row2, self.row]
+
     """
     test_build_url
     --------------
@@ -78,80 +92,128 @@ class ETATestCase(unittest.TestCase):
         }
         self.assertEquals(url, base_url + urllib.urlencode(query))
 
-    """
-    test_calculate_eta
-    ------------------
-    Tests that calculate_eta works as expected for a real request
-    from Sadler to WM Hall. Uses response recorded in the cassette file
-    """
-    @myvcr.use_cassette()
-    def test_calculate_eta(self):
-      origins = [(37.272042,-76.714027)]
-      destinations = [(37.273485,-76.719628)]
-      eta = calculate_eta(origins, destinations)
-      self.assertEquals(eta, 252)
+    def test_get_element_eta(self):
+        def make_test(element, result):
+            eta = get_element_eta(element)
+            self.assertEquals(eta, result)
+        make_test(None, None)
+        make_test({}, None)
+        make_test({u'status': u'Not Found'}, None)
+        make_test({u'status': u'OK'}, None)
+        make_test({u'status': u'OK', u'duration': {}}, None)
+        make_test(self.element1, 0)
+        make_test(self.element2, 100)
 
-    """
-    test_calculate_eta_bad_status
-    ------------------
-    Tests that calculate_eta returns None when the 'status' field
-    is not 'OK'. Uses response recorded in the cassette file
-    """
-    @myvcr.use_cassette()
-    def test_calculate_eta_bad_status(self):
-      origins = [(37.272042,-76.714027)]
-      destinations = [(0,0)]
-      eta = calculate_eta(origins, destinations)
-      self.assertEquals(eta, None)
+    def test_get_elements_eta(self):
+        def make_test(elements, result):
+            eta_list = get_elements_eta(elements)
+            self.assertEquals(eta_list, result)
+        make_test(None, None)
+        make_test([], None)
+        make_test([self.element1, None], None)
+        make_test([self.element2, None], None)
+        make_test(self.elements1, [0])
+        make_test(self.elements2, [100])
+        make_test(self.elements, [0, 100])
 
-    """
-    test_calculate_eta_bad_rows
-    ---------------------------
-    Tests that calculate_eta returns None when no 'rows' field
-    is present in the response. Uses response recorded in the cassette file
-    """
-    @myvcr.use_cassette()
-    def test_calculate_eta_bad_rows(self):
-      origins = [(0,0)]
-      destinations = origins
-      eta = calculate_eta(origins, destinations)
-      self.assertEquals(eta, None)
+    def test_get_row_eta(self):
+        def make_test(row, result):
+            eta_list = get_row_eta(row)
+            self.assertEquals(eta_list, result)
+        make_test(None, None)
+        make_test({}, None)
+        make_test(self.row1, [0])
+        make_test(self.row2, [100])
+        make_test(self.row, [0, 100])
 
-    """
-    test_calculate_eta_bad_elements
-    -------------------------------
-    Tests that calculate_eta returns None when no 'elements' field
-    is present in the response. Uses response recorded in the cassette file
-    """
-    @myvcr.use_cassette()
-    def test_calculate_eta_bad_elements(self):
-      origins = [(1,1)]
-      destinations = origins
-      eta = calculate_eta(origins, destinations)
-      self.assertEquals(eta, None)
+    def test_get_rows_eta(self):
+        def make_test(rows, num_queries, result):
+            eta_list = get_rows_eta(rows, num_queries)
+            self.assertEquals(eta_list, result)
+        make_test(None, 1, None)
+        make_test([], 1, None)
+        make_test([], 0, None)
+        make_test(self.rows1, 3, None)
+        make_test([self.row1, None], 2, None)
+        make_test(self.rows1, 1, [[0]])
+        make_test(self.rows2, 1, [[100]])
+        make_test(self.rows3, 2, [[0], [100]])
+        make_test(self.rows, 3, [[0], [100], [0, 100]])
 
-    """
-    test_calculate_eta_bad_duration
-    -------------------------------
-    Tests that calculate_eta returns None when no 'duration' field
-    is present in the response. Uses response recorded in the cassette file
-    """
-    @myvcr.use_cassette()
-    def test_calculate_eta_bad_duration(self):
-      origins = [(2,2)]
-      destinations = origins
-      eta = calculate_eta(origins, destinations)
-      self.assertEquals(eta, None)
+    # """
+    # test_calculate_eta
+    # ------------------
+    # Tests that calculate_eta works as expected for a real request
+    # from Sadler to WM Hall. Uses response recorded in the cassette file
+    # """
+    # @myvcr.use_cassette()
+    # def test_calculate_eta(self):
+    #   origins = [(37.272042,-76.714027)]
+    #   destinations = [(37.273485,-76.719628)]
+    #   eta = calculate_eta(origins, destinations)
+    #   self.assertEquals(eta, 252)
 
-    """
-    test_calculate_eta_bad_value
-    -------------------------------
-    Tests that calculate_eta returns None when no 'value' field
-    is present in the response. Uses response recorded in the cassette file
-    """
-    @myvcr.use_cassette()
-    def test_calculate_eta_bad_value(self):
-      origins = [(3,3)]
-      destinations = origins
-      eta = calculate_eta(origins, destinations)
-      self.assertEquals(eta, None)
+    # """
+    # test_calculate_eta_bad_status
+    # ------------------
+    # Tests that calculate_eta returns None when the 'status' field
+    # is not 'OK'. Uses response recorded in the cassette file
+    # """
+    # @myvcr.use_cassette()
+    # def test_calculate_eta_bad_status(self):
+    #   origins = [(37.272042,-76.714027)]
+    #   destinations = [(0,0)]
+    #   eta = calculate_eta(origins, destinations)
+    #   self.assertEquals(eta, None)
+
+    # """
+    # test_calculate_eta_bad_rows
+    # ---------------------------
+    # Tests that calculate_eta returns None when no 'rows' field
+    # is present in the response. Uses response recorded in the cassette file
+    # """
+    # @myvcr.use_cassette()
+    # def test_calculate_eta_bad_rows(self):
+    #   origins = [(0,0)]
+    #   destinations = origins
+    #   eta = calculate_eta(origins, destinations)
+    #   self.assertEquals(eta, None)
+
+    # """
+    # test_calculate_eta_bad_elements
+    # -------------------------------
+    # Tests that calculate_eta returns None when no 'elements' field
+    # is present in the response. Uses response recorded in the cassette file
+    # """
+    # @myvcr.use_cassette()
+    # def test_calculate_eta_bad_elements(self):
+    #   origins = [(1,1)]
+    #   destinations = origins
+    #   eta = calculate_eta(origins, destinations)
+    #   self.assertEquals(eta, None)
+
+    # """
+    # test_calculate_eta_bad_duration
+    # -------------------------------
+    # Tests that calculate_eta returns None when no 'duration' field
+    # is present in the response. Uses response recorded in the cassette file
+    # """
+    # @myvcr.use_cassette()
+    # def test_calculate_eta_bad_duration(self):
+    #   origins = [(2,2)]
+    #   destinations = origins
+    #   eta = calculate_eta(origins, destinations)
+    #   self.assertEquals(eta, None)
+
+    # """
+    # test_calculate_eta_bad_value
+    # -------------------------------
+    # Tests that calculate_eta returns None when no 'value' field
+    # is present in the response. Uses response recorded in the cassette file
+    # """
+    # @myvcr.use_cassette()
+    # def test_calculate_eta_bad_value(self):
+    #   origins = [(3,3)]
+    #   destinations = origins
+    #   eta = calculate_eta(origins, destinations)
+    #   self.assertEquals(eta, None)
