255ecd93829683f603968f2e496efb77439cdfa5
RyanBeatty <rvbeatty@email.wm.edu>
2015-06-06 20:19:08 -0500

    hail_ride.sh script now has valid lat/long data

--- scripts/hail_ride.sh
+++ scripts/hail_ride.sh
@@ -1,6 +1,6 @@
 # Adds a new ride request to the queue
 curl -d "num_passengers=4&\
-start_latitude=10.0&\
-start_longitude=50.1&\
-end_latitude=1.01&\
-end_longitude=5.07" 127.0.0.1:5000/rides
+start_latitude=37.273485&\
+start_longitude=-76.719628&\
+end_latitude=37.280893&\
+end_longitude=-76.719691" 127.0.0.1:5000/rides
