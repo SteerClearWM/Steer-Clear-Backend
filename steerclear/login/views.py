a96ff4b5c166c2d8d4d5502dc0cbe55eef9362c1
RyanBeatty <rvbeatty@email.wm.edu>
2015-07-19 14:07:53 -0500

    added logout. changed routing

--- steerclear/login/views.py
+++ steerclear/login/views.py
@@ -1,17 +1,17 @@
 from flask import Blueprint, flash, render_template, url_for, redirect
-from flask.ext.login import login_user, login_required
+from flask.ext.login import login_user, logout_user, login_required
 from steerclear import login_manager
 from forms import *
 from models import *
 
 
-login_bp = Blueprint('login', __name__, url_prefix='/login')
+login_bp = Blueprint('login', __name__)
 
 @login_manager.user_loader
 def user_loader(user_id):
     return User.query.get(int(user_id))
 
-@login_bp.route('/', methods=['GET', 'POST'])
+@login_bp.route('/login', methods=['GET', 'POST'])
 def login():
     form = UserForm()
     if form.validate_on_submit():
@@ -21,7 +21,13 @@ def login():
             return redirect(url_for('driver_portal.index'))
     return render_template('login.html')
 
-@login_bp.route('/test')
+@login_bp.route('/logout', methods=['GET'])
 @login_required
-def test():
+def logout():
+	logout_user()
+	return redirect(url_for('.login'))
+
+@login_bp.route('/test_login')
+@login_required
+def test_login():
 	return "Congrats, you are logged in"
