b6b5f24d54a0d3b19bb331ca93fafd0001df0d18
RyanBeatty <rvbeatty@email.wm.edu>
2015-07-19 23:17:22 -0500

    wrote a few tests for login

--- tests/login_tests/views_tests.py
+++ tests/login_tests/views_tests.py
@@ -2,13 +2,27 @@ from flask import url_for
 from flask.ext import testing
 import unittest
 from steerclear import app, db
+from steerclear.models import User
 
+# name of templates used by the login module
 LOGIN_TEMPLATE_NAME = 'login.html'
 
+"""
+SteerClearLoginTestCase
+-----------------------
+TestCase for testing the login module
+"""
 class SteerClearLoginTestCase(testing.TestCase):
 
+    # do not render templates in responses
     render_templates = False
 
+    """
+    create_app
+    ----------
+    Creates an instance of the steerclear flask app
+    and sets up testing context
+    """
     def create_app(self):
         app.config['TESTING'] = True
         app.config['SQLALCHEMY_DATABASE_URI'] = app.config['TEST_SQLALCHEMY_DATABASE_URI']
@@ -17,10 +31,13 @@ class SteerClearLoginTestCase(testing.TestCase):
     """
     setUp
     -----
-    called before each test function. Sets up flask app test
-    instance and creates new test database
+    called before each test function. creates new test database
     """
     def setUp(self):
+        self.payload = {
+            u'username': u'ryan',
+            u'password': u'1234',
+        }
         db.create_all()
 
     """
@@ -32,6 +49,32 @@ class SteerClearLoginTestCase(testing.TestCase):
         db.session.remove()
         db.drop_all()
 
+    """
+    test_get_login_page
+    -------------------
+    Tests that GET request to login route 
+    returns the login page
+    """
     def test_get_login_page(self):
-        self.client.get(url_for('login.login'))
+        response = self.client.get(url_for('login.login'))
         self.assert_template_used(LOGIN_TEMPLATE_NAME)
+        self.assertTrue(response.status_code, 200)
+
+    """
+    test_login_failure_incorrect_username
+    -------------------------------------
+    Tests that login fails if the user supplies the wrong username
+    """
+    def test_login_failure_incorrect_username(self):
+        # test with no Users in db
+        response = self.client.post(url_for('login.login'), data=self.payload)
+        self.assert_template_used(LOGIN_TEMPLATE_NAME)
+        self.assertTrue(response.status_code, 200)
+
+        # test with user that has different username but same password
+        user = User(username='kyle', password='1234')
+        db.session.add(user)
+        db.session.commit()
+        response = self.client.post(url_for('login.login'), data=self.payload)
+        self.assert_template_used(LOGIN_TEMPLATE_NAME)
+        self.assertTrue(response.status_code, 200)
