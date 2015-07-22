e3c6e602de2ba2510a827f03a7537bf8a969df6c
RyanBeatty <rvbeatty@email.wm.edu>
2015-07-21 23:04:14 -0500

    stuffs

--- steerclear/login/forms.py
+++ steerclear/login/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import Form
-from wtforms import StringField
+from wtforms import TextField, PasswordField
 from wtforms.validators import DataRequired, Length
 
 class UserForm(Form):
-	username = StringField('username', [DataRequired(), Length(min=1, max=119)])
-	password = StringField('password', [DataRequired(), Length(min=1, max=119)])
+	username = TextField('username', [DataRequired(), Length(min=1, max=119)])
+	password = PasswordField('password', [DataRequired(), Length(min=1, max=119)])
