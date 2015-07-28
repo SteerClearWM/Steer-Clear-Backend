aa1cf9c33e85f783f36944c27c2d690176ebb7fe
RyanBeatty <rvbeatty@email.wm.edu>
2015-07-27 22:20:55 -0500

    added phone number field to User model

--- steerclear/login/models.py
+++ steerclear/login/models.py
@@ -1,14 +1,17 @@
 from steerclear import db
 from flask.ext import login
+from sqlalchemy_utils.types.phone_number import PhoneNumberType
 
 class User(db.Model, login.UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(120))
+    phone_number = db.Column(PhoneNumberType, unique=True)
 
     def __repr__(self):
-        return "User(ID %r, Email %r, Password %r)" % (
+        return "User(ID %r, Email %r, Password %r, Phone %r)" % (
             self.id,
             self.email,
-            self.password
+            self.password,
+            self.phone_number.e164
         )
