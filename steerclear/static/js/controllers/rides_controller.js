730ddfd42010fe1c8d034211132e5db469869f70
newOnahtaN <ncowen@email.wm.edu>
2015-07-30 01:35:44 -0400

    included some new files

---
+++ steerclear/static/js/controllers/rides_controller.js
@@ -0,0 +1,19 @@
+app.controller('RidesController', ['$scope', 'RidesService', function($scope, RidesService){
+
+	var ride = {
+        "num_passengers": 4,
+        "start_latitude": 37.273485,
+        "start_longitude": -76.719628,
+        "end_latitude": 37.280893,
+        "end_longitude": -76.719691
+    };
+
+    RidesService.createRide(ride)
+
+	RidesService.getRides().then(function(data){
+		$scope.rides = data.rides
+	})
+
+
+
+}]);
