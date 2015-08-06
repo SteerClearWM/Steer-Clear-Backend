e112c7dfa8b919197a6b176b96338c3497397972
newOnahtaN <ncowen@email.wm.edu>
2015-08-06 18:36:55 -0400

    fixed css

--- steerclear/static/js/controllers/rides_controller.js
+++ steerclear/static/js/controllers/rides_controller.js
@@ -5,16 +5,18 @@ app.controller('RidesController', ['$scope', 'RidesService', function($scope, Ri
         "start_latitude": 37.273485,
         "start_longitude": -76.719628,
         "end_latitude": 37.280893,
-        "end_longitude": -76.719691
+        "end_longitude": -76.719691,
+        "phone": 15555555555
     };
 
-    // RidesService.createRide(ride)
+    RidesService.createRide(ride)
 
 		init = function() {
 	      RidesService.getRides().then(function(data){
 	          for (var i =0; i < data.rides.length; i++){
 	              data.rides[i].start_address = ride.start_address ? ride.start_address : "Start Address Not Found";
 	              data.rides[i].end_address = ride.end_address ? ride.end_address : "End Address Not Found";
+                data.rides[i].message = data.rides[i].start_address + ": " + data.rides[i].id
 	          };
 	          $scope.rides = data.rides;
 	    	});
