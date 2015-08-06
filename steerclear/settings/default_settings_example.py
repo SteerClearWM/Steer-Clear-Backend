0efb8c3d64743db8ab8ddcf798b21a549ea3c221
newOnahtaN <ncowen@email.wm.edu>
2015-08-06 17:13:45 -0400

    added default settings

--- steerclear/settings/default_settings_example.py
+++
@@ -1,7 +0,0 @@
-WTF_CSRF_ENABLED = False
-
-SECRET_KEY = 'generate a secret key and place here'
-
-TWILIO_ACCOUNT_SID = "Your Twilio Account SID Number Here"
-TWILIO_AUTH_TOKEN = "Your Twilio Auth Token Here"
-TWILIO_NUMBER = "Your Twilio Number Here"
