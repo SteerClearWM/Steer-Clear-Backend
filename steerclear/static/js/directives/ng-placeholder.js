84e170970df148be574787b0c87da274b237025d
newOnahtaN <ncowen@email.wm.edu>
2015-08-08 18:18:49 -0400

    added directive for using angular binding in bootstrap placeholder

---
+++ steerclear/static/js/directives/ng-placeholder.js
@@ -0,0 +1,13 @@
+app.directive('ngPlaceholder', function($document) {
+  return {
+    restrict: 'A',
+    scope: {
+      placeholder: '=ngPlaceholder'
+    },
+    link: function(scope, elem, attr) {
+      scope.$watch('placeholder',function() {
+        elem[0].placeholder = scope.placeholder;
+      });
+    }
+  }
+});
