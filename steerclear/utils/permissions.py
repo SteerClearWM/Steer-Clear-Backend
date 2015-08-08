14c6f7ecdddad54b54574753df5aaa9cb52a6853
RyanBeatty <rvbeatty@email.wm.edu>
2015-08-07 22:18:04 -0500

    added granular ride permission

--- steerclear/utils/permissions.py
+++ steerclear/utils/permissions.py
@@ -1,6 +1,24 @@
 from flask.ext.principal import Permission, RoleNeed
+from collections import namedtuple
+from functools import partial
 
-
+# Permission requiring that User must be admin to access resource
 admin_permission = Permission(RoleNeed('admin'))
+
+# Permission requiring User to be student to access resource
 student_permission = Permission(RoleNeed('student'))
+
+# Permission requiring User to be a student or an admin to access resource
 student_or_admin_permission = Permission(RoleNeed('student'), RoleNeed('admin'))
+
+
+RideNeed = namedtuple('ride', ['method', 'value'])
+AccessRideNeed = partial(RideNeed, 'access')
+
+class AccessRidePermission(Permission):
+	def __init__(self, ride_id):
+		need = AccessRideNeed(unicode(ride_id))
+		super(AccessRidePermission, self).__init__(need)
+
+
+
