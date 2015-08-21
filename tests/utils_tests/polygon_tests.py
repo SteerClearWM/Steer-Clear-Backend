ae5252e103c8ba53c7205be2b0d724ed27d64f5a
RyanBeatty <rvbeatty@email.wm.edu>
2015-08-20 21:18:48 -0400

    testing polygon

--- tests/utils_tests/polygon_tests.py
+++ tests/utils_tests/polygon_tests.py
@@ -19,57 +19,57 @@ class SteerClearGISClientTestCase(unittest.TestCase):
 
     def test_is_on_campus_points_in_center_of_campus(self):
         # test with lat/long of william and mary
-        result = self.gis_client.is_on_campus((37.272433, -76.716922))
-        self.assertTrue(result)
+        self._test_point((37.272433, -76.716922), True)
 
         # test with lat/long of sadler center
-        result = self.gis_client.is_on_campus((37.271819, -76.714061))
+        self._test_point((37.271819, -76.714061), True)
+
+        # test point near randolph complex
+        self._test_point((37.270857, -76.719126), True)
+
+        # test point near swem library
+        self._test_point((37.269832, -76.716803), True)
+
+        # test point near botetourt
+        self._test_point((37.270255, -76.721078), True)
 
     def test_is_on_campus_points_near_perimeter_of_campus(self):
         # test point near barret hall
-        result = self.gis_client.is_on_campus((37.269028, -76.711410))
-        self.assertTrue(result)
+        self._test_point((37.269028, -76.711410), True)
 
         # test point near jefferson hall
-        result = self.gis_client.is_on_campus((37.269501, -76.710186))
-        self.assertTrue(result)
+        self._test_point((37.269501, -76.710186), True)
 
         # test point near colonial williamsburg but still on campus
-        result = self.gis_client.is_on_campus((37.270837, -76.707575))
-        self.assertTrue(result)
+        self._test_point((37.270837, -76.707575), True)
 
         # test point near blow hall
-        result = self.gis_client.is_on_campus((37.272114, -76.709993))
-        self.assertTrue(result)
+        self._test_point((37.272114, -76.709993), True)
 
         # test point near bryan hall
-        result = self.gis_client.is_on_campus((37.272938, -76.712275))
-        self.assertTrue(result)
+        self._test_point((37.272938, -76.712275), True)
 
         # test point near zable hall
-        result = self.gis_client.is_on_campus((37.273959, -76.713427))
-        self.assertTrue(result)
+        self._test_point((37.273959, -76.713427), True)
 
         # test point near frat castles
-        result = self.gis_client.is_on_campus((37.274754, -76.718091))
-        self.assertTrue(result)
+        self._test_point((37.274754, -76.718091), True)
 
         # test point near rec center
-        result = self.gis_client.is_on_campus((37.274705, -76.720931))
-        self.assertTrue(result)
-
-        # test point near randolph complex
-        result = self.gis_client.is_on_campus((37.270857, -76.719126))
-        self.assertTrue(result)
+        self._test_point((37.274705, -76.720931), True)
 
         # test point near jamestown south
-        result = self.gis_client.is_on_campus((37.268358, -76.712992))
-        self.assertTrue(result)
+        self._test_point((37.268358, -76.712992), True)
 
         # test point near PBK
-        result = self.gis_client.is_on_campus((37.267523, -76.714738))
-        self.assertTrue(result)
+        self._test_point((37.267523, -76.714738), True)
 
         # test point near business school
-        result = self.gis_client.is_on_campus((37.266099, -76.717825))
-        self.assertTrue(result)
+        self._test_point((37.266099, -76.717825), True)
+
+    def test_is_on_campus_points_not_on_campus(self):
+        pass
+
+    def _test_point(self, point, result):
+        r = self.gis_client.is_on_campus(point)
+        self.assertEqual(r, result)
