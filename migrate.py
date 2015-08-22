31238510d58913629c6b7bfd65d0203144ce0f50
RyanBeatty <rvbeatty@email.wm.edu>
2015-08-22 00:14:29 -0400

    added documentation to migrate.py

--- migrate.py
+++ migrate.py
@@ -2,6 +2,12 @@ from steerclear import app, db
 from flask.ext.script import Manager
 from flask.ext.migrate import Migrate, MigrateCommand
 
+"""
+migrate
+-------
+Enable database creation/migration
+taken from: https://flask-migrate.readthedocs.org/en/latest/
+"""
 def migrate():
     migrate = Migrate(app, db)
 
