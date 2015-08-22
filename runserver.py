55b1e562a36485c7be26f4365e5a117ecfe62aff
RyanBeatty <rvbeatty@email.wm.edu>
2015-08-22 00:39:33 -0400

    added command line arguments to runserver for running in production

--- runserver.py
+++ runserver.py
@@ -1,4 +1,13 @@
 from steerclear import app
+import argparse
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    # set up command line argument parser
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--port', dest='port', type=int, default=5000, 
+                        help='port number to run app on')
+    parser.add_argument('--debug', dest='debug', action='store_true', default=False)
+    args = parser.parse_args()
+
+    # run flask app with configurations
+    app.run(port=args.port, debug=args.debug)
