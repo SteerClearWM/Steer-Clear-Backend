b899a42dfef932351f4b2c5a3d65edda24cfa906
newOnahtaN <ncowen@email.wm.edu>
2015-08-29 21:04:38 -0400

    supporting both start and end addressing

--- steerclear/static/js/controllers/rides_controller.js
+++ steerclear/static/js/controllers/rides_controller.js
@@ -5,7 +5,7 @@ app.controller('RidesController', ['$scope', 'RidesService', function($scope, Ri
         "start_latitude": 37.273485,
         "start_longitude": -76.719628,
         "end_latitude": 37.280893,
-        "end_longitude": -76.719691,
+        "end_longitude": -76.1,
         "phone": 15555555555
     };
 
@@ -14,15 +14,16 @@ app.controller('RidesController', ['$scope', 'RidesService', function($scope, Ri
 		init = function() {
 	      RidesService.getRides().then(function(data){
 	          for (var i =0; i < data.rides.length; i++){
-	              data.rides[i].start_address = ride.start_address ? ride.start_address : "Start Address Not Found";
-	              data.rides[i].end_address = ride.end_address ? ride.end_address : "End Address Not Found";
-                data.rides[i].message = data.rides[i].start_address + ": " + data.rides[i].id
+	              data.rides[i].pickup_address = data.rides[i].pickup_address || "Start Address Not Found";
+	              data.rides[i].dropoff_address = data.rides[i].dropoff_address || "End Address Not Found";
 	          };
 	          $scope.rides = data.rides;
 	    	});
     };
 
     init();
+
+    console.log($scope.rides);
 
     $scope.deleteRide = function ( ride ) {
         if (confirm("Are you sure you want to delete this ride?")){
