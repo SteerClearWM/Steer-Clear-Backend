4b9b4c70d11d0acc80b330b9ffbf6a470ea1f5a8
RyanBeatty <rvbeatty@email.wm.edu>
2016-02-20 01:10:21 -0500

    permissions_tests working

--- tests/utils_tests/permissions_tests.py
+++ tests/utils_tests/permissions_tests.py
@@ -32,3 +32,19 @@ class SteerClearPermissionsTestCase(base.SteerClearBaseTestCase):
         for student in students:
             self.assertIn(self.student_role, student.roles)
 
+    """
+    test_multiple_users_can_have_admin_permission
+    -----------------------------------------------
+    Tests that multiple users can have the admin permission
+    """
+    def test_multiple_users_can_have_admin_permission(self):
+        # create new users and give all of them the student permission
+        admin1 = self._create_user(username='bob', phone='+15554443333', role=self.admin_role)
+        admin2 = self._create_user(username='kat', phone='+12223334445', role=self.admin_role)
+        admin3 = self._create_user(username='ron', phone='+12223334446', role=self.admin_role)
+        admins = [admin1, admin2, admin3]
+
+        # Check that each user has the admin permission
+        for admin in admins:
+            self.assertIn(self.admin_role, admin.roles)
+
