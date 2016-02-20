dba44216113737d2ef8de33249c4e802aaec9c32
RyanBeatty <rvbeatty@email.wm.edu>
2016-02-19 23:24:12 -0500

    started bug fix for issue #53

---
+++ tests/utils_tests/permissions_tests.py
@@ -0,0 +1,34 @@
+from tests.base import base
+from steerclear.models import User
+
+"""
+SteerClearPermissionsTestCase
+-----------------------
+TestCase for testing permissions
+"""
+class SteerClearPermissionsTestCase(base.SteerClearBaseTestCase):
+
+    """
+    setUp
+    -----
+    called before each test function. creates new test database
+    """
+    def setUp(self):
+        super(SteerClearPermissionsTestCase, self).setUp()
+
+    """
+    test_multiple_users_can_have_student_permission
+    -----------------------------------------------
+    Tests that multiple users can have the student permission
+    """
+    def test_multiple_users_can_have_student_permission(self):
+        # create new users and give all of them the student permission
+        student1 = self._create_user(username='bob', phone='+15554443333', role=self.student_role)
+        student2 = self._create_user(username='kat', phone='+12223334445', role=self.student_role)
+        student3 = self._create_user(username='ron', phone='+12223334446', role=self.student_role)
+        students = [student1, student2, student3]
+
+        # Check that each user has the student permission
+        for student in students:
+            self.assertIn(self.student_role, student.roles.all())
+
